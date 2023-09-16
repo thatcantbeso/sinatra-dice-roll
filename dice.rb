2fa09b2e30feb5466b44087ccbbc446bf6f76fba
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-09-16 00:17:16 +0000

    fixed headers

--- dice.rb
+++ dice.rb
@@ -6,6 +6,9 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+get("/") do
+  erb(:elephant)
+end
 
 get("/") do
   "
@@ -36,10 +39,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+   erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -47,20 +49,18 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d10</h1>
-   <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
   first_die = rand(1..20)
   sum = first_die
 	
-  outcome = "You rolled a #{first_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} for a total of #{sum}."
 	
-  "<h1>1d20</h1>
-   <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -70,8 +70,7 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die 
 	
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, and a #{fourth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-  "<h1>5/4</h1>
-   <p>#{outcome}</p>"
+  erb(:five_four)
 end
